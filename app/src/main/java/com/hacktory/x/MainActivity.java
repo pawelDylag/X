360d1016181df56475c65e0312d0863a557d15f9
VaccumMan <lukmar93@o2.pl>
2015-11-07 12:54:12 +0100

    Merge branch 'master' of https://github.com/pawelDylag/X
    Conflicts:
    	app/src/main/java/com/hacktory/x/MainActivity.java
    	app/src/main/java/com/hacktory/x/MainFragment.java

--- app/src/main/java/com/hacktory/x/MainActivity.java
+++ app/src/main/java/com/hacktory/x/MainActivity.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
 import com.estimote.sdk.Beacon;
 import com.estimote.sdk.BeaconManager;
