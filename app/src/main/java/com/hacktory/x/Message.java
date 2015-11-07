fb1620751d3fdefacea58a47382debe43e481d7c
Paweł Dyląg <p.dyloo@gmail.com>
2015-11-07 11:49:32 +0100

    Added butterknife

---
+++ app/src/main/java/com/hacktory/x/Message.java
@@ -0,0 +1,16 @@
+package com.hacktory.x;
+
+/**
+ * Pojedyncza wiadomosc
+ * Created by paweldylag on 07/11/15.
+ */
+public class Message {
+
+    private String message;
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+
+}
--- app/src/main/java/com/hacktory/x/receive/MessageFragment.java
+++ app/src/main/java/com/hacktory/x/receive/MessageFragment.java
@@ -1,14 +1,17 @@
 package com.hacktory.x.receive;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.design.widget.CoordinatorLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ListAdapter;
 import android.widget.TextView;
 
@@ -16,6 +19,20 @@ import com.hacktory.x.R;
 
 public class MessageFragment extends Fragment {
 
+    /**
+     * Board view object
+     */
+    private GridView boardView;
+
+    /**
+     * Coordinator layout for displaying messages on screen
+     */
+    private CoordinatorLayout coordinatorLayout;
+
+    /**
+     * Current activity context
+     */
+    private Context context;
 
     /**
      * The fragment's ListView/GridView.
@@ -44,6 +61,21 @@ public class MessageFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+    }
+
+    private void initBoardView() {
+        // set adapter for grid view
+        boardView.setAdapter(boardAdapter);
+        // set grid view params
+        boardView.setNumColumns(boardSize);
+        boardView.setColumnWidth(BOARD_CELL_SIZE_DP);
+        // initial animation
+        Animation cellAnimation = AnimationUtils.loadAnimation(context, R.anim.board_cell_appear);
+        GridLayoutAnimationController animController = new GridLayoutAnimationController(cellAnimation);
+        animController.setDirectionPriority(GridLayoutAnimationController.PRIORITY_NONE);
+        animController.setColumnDelay(0.6f / boardSize);
+        animController.setRowDelay(0.6f / boardSize);
+        boardView.setLayoutAnimation(animController);
     }
 
     @Override
