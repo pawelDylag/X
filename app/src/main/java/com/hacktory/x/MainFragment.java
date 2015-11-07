58b38ee50ac2ab24429065114203601fa91e4546
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2015-11-07 13:33:18 +0100

    main fragment

--- app/src/main/java/com/hacktory/x/MainFragment.java
+++ app/src/main/java/com/hacktory/x/MainFragment.java
@@ -33,7 +33,7 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_range, container, false);
+        View view =  inflater.inflate(R.layout.fragment_main, container, false);
 
         getViews(view);
         setListeners();
