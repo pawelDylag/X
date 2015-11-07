de0247a41eaac9bc737ee4010460c1f7eaeffc22
VaccumMan <lukmar93@o2.pl>
2015-11-07 13:33:26 +0100

    beaconHelper for check current trace

--- app/src/main/java/com/hacktory/x/receive/ReceiveFragment.java
+++ app/src/main/java/com/hacktory/x/receive/ReceiveFragment.java
@@ -1,20 +1,20 @@
 package com.hacktory.x.receive;
 
+import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-import com.hacktory.x.data.Message;
 import com.hacktory.x.R;
+import com.hacktory.x.data.Message;
 
 import java.util.ArrayList;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class ReceiveFragment extends Fragment {
 
@@ -49,6 +49,7 @@ public class ReceiveFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ButterKnife.bind(recyclerView);
         messageListAdapter = new MessageListAdapter(new ArrayList<Message>());
         recyclerView.setAdapter(messageListAdapter);
 
