2cfac61091c5d10435ecb8a78d3c7133cc894896
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2015-11-07 12:03:42 +0100

    toolbar fix

--- app/src/main/java/com/hacktory/x/MainActivity.java
+++ app/src/main/java/com/hacktory/x/MainActivity.java
@@ -28,8 +28,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         setFragment(Constants.FRAGMENT_MAIN);
         setupEstimoteSDK();
