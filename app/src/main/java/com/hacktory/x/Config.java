1140020de17d8051372113f02c80becd3084b592
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2015-11-07 12:00:40 +0100

    added config, placeholder layout

---
+++ app/src/main/java/com/hacktory/x/Config.java
@@ -0,0 +1,12 @@
+package com.hacktory.x;
+
+/**
+ * Created by Patryk Mieczkowski on 07.11.15
+ */
+public class Config {
+
+    public static final int FRAGMENT_MAIN = 0;
+    public static final int FRAGMENT_RECEIVE = 1;
+    public static final int FRAGMENT_SEND = 2;
+
+}
--- app/src/main/java/com/hacktory/x/MainActivity.java
+++ app/src/main/java/com/hacktory/x/MainActivity.java
@@ -17,15 +17,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        setFragment(Config.FRAGMENT_MAIN);
         setupEstimoteSDK();
+    }
+
+    private void setFragment(int selectedFragment){
+        Log.d(TAG, "selected fragment: " + selectedFragment);
+        
     }
 
     private void setupEstimoteSDK() {
