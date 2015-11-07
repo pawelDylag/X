49bf8a786edccf9721b5d3432487f9a974763355
abiela <biela.arek@gmail.com>
2015-11-07 12:04:48 +0100

    Add p2p Manager and channels, create broadcast receiver

--- app/src/main/java/com/hacktory/x/MainActivity.java
+++ app/src/main/java/com/hacktory/x/MainActivity.java
@@ -1,5 +1,11 @@
 package com.hacktory.x;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -7,8 +13,16 @@ import android.util.Log;
 
 import com.estimote.sdk.EstimoteSDK;
 
+
+
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
+
+    private IntentFilter intentFilter = new IntentFilter();
+    private BroadcastReceiver broadcastReceiver;
+
+    WifiP2pManager.Channel p2pChannel;
+    WifiP2pManager p2pManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +40,72 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
         setupEstimoteSDK();
+        initIntentFilters();
+        initP2PChannel();
+        initBroadcastReceiver();
+        registerReceiver();
     }
 
     private void setupEstimoteSDK() {
         Log.d(TAG, "setupEstimoteSDK ");
         EstimoteSDK.initialize(this, "estimons-mzy", "e2c71dee0a386b6a548d0cde0754384a");
+    }
 
+    private void initIntentFilters() {
+        Log.d(TAG, "initIntentFilters() called with: " + "");
 
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+    }
+
+    private void initP2PChannel() {
+        Log.d(TAG, "initP2PChannel() called with: " + "");
+
+        p2pManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        p2pChannel = p2pManager.initialize(this, getMainLooper(), null);
+    }
+
+    private void initBroadcastReceiver() {
+        Log.d(TAG, "initBroadcastReceiver() called with: " + "");
+
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                String action = intent.getAction();
+
+                if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+                    // Determine if Wifi P2P mode is enabled or not, alert
+                    // the Activity.
+                    int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+                    if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                        Log.d(TAG, "Wifi_P2P_enabled !");
+                    } else {
+                        Log.d(TAG, "Wifi_P2P_disabled !");
+                    }
+                } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+                    Log.d(TAG, "Wifi_P2P peers changed !");
+                    // The peer list has changed!  We should probably do something about
+                    // that.
+
+                } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+
+                    // Connection state changed!  We should probably do something about
+                    // that.
+
+                } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+
+                }
+            }
+        };
+    }
+
+    private void registerReceiver() {
+        Log.d(TAG, "registerReceiver() called with: " + "");
+
+        registerReceiver(broadcastReceiver, intentFilter);
     }
 
 }
